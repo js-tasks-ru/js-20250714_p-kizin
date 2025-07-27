a10c7e6352e7dba39b62194d426339e4815fe300
Pavel Kizin <p.p.kizin@gmail.com>
2025-07-27 18:13:29 +0300

    Add Tooltip implementation

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,59 @@
 class Tooltip {
-  initialize () {
+  static #singleton;
 
+  static #element;
+  get element() { return Tooltip.#element; }
+
+  constructor() {
+    if (!Tooltip.#singleton) {
+      Tooltip.#singleton = this;
+    }
+
+    return Tooltip.#singleton;
+  }
+
+  initialize () {
+    if (Tooltip.#element) { return; }
+    
+    const tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip');
+
+    Tooltip.#element = tooltip;
+
+    const render = (message) => this.render(message);
+
+    const remove = () => this.remove();
+
+    document.addEventListener('pointerover', function (event) {
+      if (event.target.dataset.tooltip != undefined) {
+        render(event.target.dataset.tooltip);
+      }
+    });
+
+    document.addEventListener('pointerout', function (event) {
+      if (event.target.dataset.tooltip != undefined) {
+        remove();
+      }
+    });
+  }
+
+  render(message = '') {
+    if (!Tooltip.#element) { return; }
+
+    Tooltip.#element.textContent = message;
+
+    const container = document.getElementById('container');
+    container.append(Tooltip.#element);
+  }
+
+  remove() {
+    if (!Tooltip.#element) { return; }
+    
+    Tooltip.#element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
